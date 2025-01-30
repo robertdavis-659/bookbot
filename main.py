2b39e2c0d39578f3c3faaae6b0c7342d631d74a1
robertdavis-659 <d6robert@gmail.com>
2025-01-29 22:10:01 -0500

    added functions to return character counts, letter counts sorted by value; formatted print statements to deliver terminal report

--- main.py
+++ main.py
@@ -2,18 +2,49 @@ def main():
     file_path = "books/frankenstein.txt"
     text = get_file_text(file_path)
     word_count = get_word_count(text)
-    print(text)
+    character_counts = get_character_counts(text)
+    letter_counts = get_sorted_letter_counts(character_counts)
+#    print(text)
+    print(f"--- Begin report of {file_path} ---")
     print(f"This file contains {word_count} word(s)")
+#    print(character_counts)
+#    print(letter_counts)
+    for keys in letter_counts.keys():
+        print(f"The letter '{keys}' appears {letter_counts[keys]} times")
+    print("--- End report ---")
 
-
+# This function takes a provided file path and returns a string of the file's contents.
+# main() sets the variable "text" to the string returned by this function.
 def get_file_text(path):
     with open(path) as file:
         return file.read()
 
-
+# This function takes a provided string and splits the string into a list of individual words called words.
+# It then returns the length of the words list (count of items in the list).
+# main() sets the variable "word_count" to the number returned by this function.
 def get_word_count(text):
     words = text.split()
     return len(words)
 
+# This function takes a provided string and converts it into a list of characters. Those characters are corrected to be in all lower case.
+# It then creates a dicationary (character_counts) by iterating over the list to add each unique character to the dictionary as a key with a value of 0.
+# The function loops over each character in the characters list and increments the value for that character in the dictionary.
+# This function returns the dictionary character_counts which has a key for each unique character in the provided text and a value counting each time that character appeared in the text.
+# main() sets the variable character_counts to the dictionary returned by this function
+def get_character_counts(text):
+    characters = list(text.lower())
+    character_counts = {char:0 for char in characters}
+    for char in characters:
+        character_counts[char] += 1
+    return character_counts
+
+#
+def get_sorted_letter_counts(character_counts):
+    letter_counts = {}
+    for key, value in character_counts.items():
+        if key.isalpha() == True:
+            letter_counts[key] = value
+    letter_counts = {key: value for key, value in sorted(letter_counts.items(), key=lambda kv: (kv[1], kv [0]), reverse = True)}
+    return letter_counts
 
 main()
